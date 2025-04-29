4f8eb7a28dfaa0ab5cef15e7b03a7543112e7d82
reddy-sravan12 <101791343+reddy-sravan12@users.noreply.github.com>
2025-04-29 22:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If required
+function daysOfAYear(year){
+	if(year%400===0(year%100!==0&&year%400===0)){
+		return 366
+	}
+	return 365
+}
+
+// alert("enter year")
